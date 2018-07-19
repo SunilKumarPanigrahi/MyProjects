494c74adaab8611c1cc12709e34e5f8619771f44
Sunil Kumar Panigrahi <39371809+SunilKumarPanigrahi@users.noreply.github.com>
2018-07-19 20:46:32 +0530

    Add files via upload

---
+++ APPS/APP1.py
@@ -0,0 +1,8 @@
+print('===================================')
+print('===================================')
+print('Hello  App')
+print('===================================')
+
+Listen = input('What is your name ?')
+print('Welcome To My World ' + Listen + ' Click start to proceed')
+
