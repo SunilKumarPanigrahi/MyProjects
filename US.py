564f95ac944302be6399121f167d97d621d0b337
Sunil Kumar Panigrahi <39371809+SunilKumarPanigrahi@users.noreply.github.com>
2018-07-19 18:30:34 +0530

    Add files via upload
    This is  just a welcome note.

---
+++ US.py
@@ -0,0 +1,7 @@
+print("=================================================")
+print("          Hello App    ")
+print("=================================================")
+saying = input("What is your name?")
+print("Welcome Back " + saying + " How are you today ?")
+
+
